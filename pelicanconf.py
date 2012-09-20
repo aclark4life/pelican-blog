667f11e4d16dd23804ae309eecabbb80786dcd50
Alex Clark <aclark@aclark.net>
2012-09-20 19:30:32 -0400

    Remove blogroll ; add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,20 +3,22 @@
 
 AUTHOR = u"aclark@aclark.net"
 SITENAME = u"Alex Clark"
-SITEURL = ''
+SITEURL = 'http://blog.aclark.net'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
-
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+SOCIAL = (('Twitter', 'http://twitter.com/aclark4life'),
+          ('GitHub', 'http://github.com/aclark4life'),)
 
 DEFAULT_PAGINATION = 10
