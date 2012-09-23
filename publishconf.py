c0eb7011197c531bc56c4a745347d14d1cbc4676
Alex Clark <aclark@aclark.net>
2012-09-22 21:04:32 -0400

    Publish

--- publishconf.py
+++ publishconf.py
@@ -1,42 +1,29 @@
-
-AUTHOR = u"Alex Clark"
-SITENAME = u"Alex Clark"
-SITEURL = 'http://blog.aclark.net'
+ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/%d/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+ARTICLE_URL = '{slug}'
+AUTHOR = u'Alex Clark'
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+DEFAULT_CATEGORY = 'Blog'
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+DELETE_OUTPUT_DIRECTORY = True
+DISQUS_SITENAME = 'aclark-blog'
+GITHUB_URL = 'https://github.com/ACLARKNET/aclarknet.github.com'
+GOOGLE_ANALYTICS = 'UA-34988446-1'
+RELATIVE_URLS = False
+SITENAME = u'Alex Clark'
+SITEURL = 'http://blog.aclark.net'
+SITEURL = 'http://blog.aclark.net'
 SOCIAL = (
-    ('atom feed (Mozilla)', 'http://blog.aclark.net/Mozilla.atom.xml'),
-    ('atom feed (Plone)', 'http://blog.aclark.net/Plone.atom.xml'),
-    ('atom feed (Python)', 'http://blog.aclark.net/Python.atom.xml'),
     ('GitHub', 'http://github.com/aclark4life'),
     ('Gittip', 'https://www.gittip.com/aclark4life'),
     ('PythonPackages', 'https://pythonpackages.com/user/aclark4life'),
     ('Twitter', 'http://twitter.com/aclark4life'),
+    ('atom feed (Mozilla)', 'http://blog.aclark.net/Mozilla.atom.xml'),
+    ('atom feed (Plone)', 'http://blog.aclark.net/Plone.atom.xml'),
+    ('atom feed (Python)', 'http://blog.aclark.net/Python.atom.xml'),
 )
-
-DEFAULT_PAGINATION = 10
-
-# Default category AKA header configuration
-DEFAULT_CATEGORY = "Blog"
-
-# Generate feeds for tags instead of categories
-CATEGORY_FEED_ATOM = None
-CATEGORY_FEED_RSS = None
 TAG_FEED_ATOM = 'feeds/%s.atom.xml'
 TAG_FEED_RSS = None
-
-# Generate fancy permalinks
-ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/%d/'
-# Disable .html
-ARTICLE_URL = '{slug}'
-ARTICLE_SAVE_AS = "{slug}/index.html"
-
-SITEURL = 'http://blog.aclark.net'
-
-DELETE_OUTPUT_DIRECTORY = True
-
-RELATIVE_URLS = False
-
-DISQUS_SITENAME = "aclark-blog"
-GOOGLE_ANALYTICS = "UA-34988446-1"
-GITHUB_URL = "https://github.com/ACLARKNET/aclarknet.github.com"
 TWITTER_USERNAME = 'aclark4life'
