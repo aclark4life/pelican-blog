6615b427a3eb821db67782148d6cc9014b736874
Alex Clark <aclark@aclark.net>
2012-09-22 21:00:45 -0400

    Move everything to publish conf

--- publishconf.py
+++ publishconf.py
@@ -1,18 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
 
-import sys
-sys.path.append('.')
-from pelicanconf import *
+AUTHOR = u"Alex Clark"
+SITENAME = u"Alex Clark"
+SITEURL = 'http://blog.aclark.net'
+DEFAULT_LANG = 'en'
+SOCIAL = (
+    ('atom feed (Mozilla)', 'http://blog.aclark.net/Mozilla.atom.xml'),
+    ('atom feed (Plone)', 'http://blog.aclark.net/Plone.atom.xml'),
+    ('atom feed (Python)', 'http://blog.aclark.net/Python.atom.xml'),
+    ('GitHub', 'http://github.com/aclark4life'),
+    ('Gittip', 'https://www.gittip.com/aclark4life'),
+    ('PythonPackages', 'https://pythonpackages.com/user/aclark4life'),
+    ('Twitter', 'http://twitter.com/aclark4life'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Default category AKA header configuration
+DEFAULT_CATEGORY = "Blog"
+
+# Generate feeds for tags instead of categories
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+TAG_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_RSS = None
+
+# Generate fancy permalinks
+ARTICLE_PERMALINK_STRUCTURE = '/%Y/%m/%d/'
+# Disable .html
+ARTICLE_URL = '{slug}'
+ARTICLE_SAVE_AS = "{slug}/index.html"
 
 SITEURL = 'http://blog.aclark.net'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
+RELATIVE_URLS = False
 
 DISQUS_SITENAME = "aclark-blog"
 GOOGLE_ANALYTICS = "UA-34988446-1"
