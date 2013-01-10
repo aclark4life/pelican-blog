9bcec0c93735cd6355874014a3ef49d43fb42c0c
Alex Clark <aclark@aclark.net>
2013-01-10 15:39:42 -0500

    Fix conf

--- publishconf.py
+++ publishconf.py
@@ -11,11 +11,11 @@ DISQUS_SITENAME = 'aclark-blog'
 FEED = 'feeds/all.atom.xml'
 GITHUB_URL = 'https://github.com/ACLARKNET/aclarknet.github.com'
 GOOGLE_ANALYTICS = 'UA-34988446-1'
-SITENAME = u'Alex Clark'
+SITENAME = u'Alex Clark - Python Web Developer'
 SITEURL = 'http://blog.aclark.net'
 SOCIAL = (
     ('aclark4life', 'http://twitter.com/aclark4life'),
-    ('Gittip', 'https://www.gittip.com/aclark4life'),
+    ('aclark4life (Gittip)', 'https://www.gittip.com/aclark4life'),
     ('atom feed (Django)', 'http://blog.aclark.net/feeds/Django.atom.xml'),
     ('atom feed (Mozilla)', 'http://blog.aclark.net/feeds/Mozilla.atom.xml'),
     ('atom feed (Plone)', 'http://blog.aclark.net/feeds/Plone.atom.xml'),
