4abcdc2868375f9c961151ce05b8cf17eecd7c3c
Alex Clark <aclark@aclark.net>
2013-01-10 15:37:02 -0500

    Publish

--- publishconf.py
+++ publishconf.py
@@ -1,23 +1,21 @@
 ARTICLE_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 ARTICLE_URL = '/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 AUTHOR = u'aclark'
-CATEGORY_FEED_ATOM = 'http://blog.aclark.net/feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = None
 CATEGORY_FEED_RSS = None
 DEFAULT_CATEGORY = 'Blog'
 DEFAULT_LANG = 'en'
 DEFAULT_PAGINATION = 10
 DELETE_OUTPUT_DIRECTORY = True
 DISQUS_SITENAME = 'aclark-blog'
+FEED = 'feeds/all.atom.xml'
 GITHUB_URL = 'https://github.com/ACLARKNET/aclarknet.github.com'
 GOOGLE_ANALYTICS = 'UA-34988446-1'
 SITENAME = u'Alex Clark'
 SITEURL = 'http://blog.aclark.net'
 SOCIAL = (
     ('aclark4life', 'http://twitter.com/aclark4life'),
-    ('ACLARKNET', 'http://twitter.com/ACLARKNET'),
-    ('GitHub', 'http://github.com/aclark4life'),
     ('Gittip', 'https://www.gittip.com/aclark4life'),
-    ('PythonPackages', 'https://pythonpackages.com/user/aclark4life'),
     ('atom feed (Django)', 'http://blog.aclark.net/feeds/Django.atom.xml'),
     ('atom feed (Mozilla)', 'http://blog.aclark.net/feeds/Mozilla.atom.xml'),
     ('atom feed (Plone)', 'http://blog.aclark.net/feeds/Plone.atom.xml'),
